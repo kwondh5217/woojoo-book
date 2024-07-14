8c2deef5a37f9654fd054bb4ea25a9fbaecdc419
kwondh5217 <trewq231@naver.com>
2024-07-15 03:05:58 +0900

    Feat : 인증용 회원 정보 생성,검증 기능 구현

--- src/main/java/com/ssafy11/woojoobook/member/MemberService.java
+++ src/main/java/com/ssafy11/woojoobook/member/MemberService.java
@@ -1,10 +1,31 @@
 package com.ssafy11.woojoobook.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MemberService {
 
+    private final MemberVerificationRepository memberVerificationRepository;
 
+    public boolean verifyMember(String email, String code) {
+        MemberVerification memberVerification = this.memberVerificationRepository.findById(email)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 email"));
+        memberVerification.verify(code);
 
+        MemberVerification save = this.memberVerificationRepository.save(memberVerification);
+
+        return save.isVerified();
+    }
+
+    public void createVerificationMember(String email){
+        MemberVerification memberVerification = MemberVerification.builder()
+                .email(email)
+                .verificationCode(VerificationCodeUtil.generate())
+                .isVerified(false)
+                .build();
+
+        this.memberVerificationRepository.save(memberVerification);
+    }
 }
