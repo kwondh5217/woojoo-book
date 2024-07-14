53b8a1833885c45c6d1eb98b20d8570ef3ed5e9a
kwondh5217 <trewq231@naver.com>
2024-07-15 03:06:18 +0900

    Feat : 인증용 회원 리포지토리 구현

---
+++ src/main/java/com/ssafy11/woojoobook/member/MemberVerificationRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy11.woojoobook.member;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberVerificationRepository extends CrudRepository<MemberVerification, String> {
+
+}
